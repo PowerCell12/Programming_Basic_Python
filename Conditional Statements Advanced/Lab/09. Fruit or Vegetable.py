aa867cb719aac4d4f3bde93099bbb3c0f58e7d6e
Ivan <87208452+PowerCell12@users.noreply.github.com>
2023-01-19 22:22:28 +0200

    Create 09. Fruit or Vegetable.py

---
+++ Conditional Statements Advanced/Lab/09. Fruit or Vegetable.py
@@ -0,0 +1,10 @@
+product = str(input())
+
+if product == "banana" or product == "apple" or product == "kiwi" or product == "cherry" or product == "lemon" or product == "grapes":
+    print("fruit")
+
+if product == "tomato" or product == "cucumber" or product == "pepper" or product == "carrot":
+    print("vegetable")
+
+if product != "banana" and product != "apple" and product != "kiwi" and product != "cherry" and product != "lemon" and product != "grapes" and product != "tomato" and product != "cucumber" and product != "pepper" and product != "carrot":
+    print("unknown")
