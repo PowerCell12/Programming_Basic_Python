f289f1cc115c33b9b2fdaccc7c5c9391614ca037
Ivan <87208452+PowerCell12@users.noreply.github.com>
2023-02-21 23:27:32 +0200

    Create 03. Combinations.py

---
+++ Nested Loops/Lab/03. Combinations.py
@@ -0,0 +1,10 @@
+number = int(input())
+how_may_times = 0
+
+for x1 in range(0, number + 1):
+    for x2 in range(0, number + 1):
+        for x3 in range(0, number + 1):
+            if x1 + x2 + x3 == number:
+                how_may_times += 1
+
+print(how_may_times)
